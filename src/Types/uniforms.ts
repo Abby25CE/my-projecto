cf28838a868b2a2d1981085a23443cbb86070de0
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2025-07-14 20:14:35 -0600

    Update uniforms.ts

--- src/Types/uniforms.ts
+++ src/Types/uniforms.ts
@@ -20,3 +20,36 @@ export interface UniformProps {
     colors: string;
   };
 }
+
+export interface AddressData {
+  id?: number;
+  nombre: string;
+  direccion: string;
+  numero: number;
+  referencia: string;
+  municipio: string;
+  curp: string;
+  correo: string;
+  telefono: number;
+  colonia: string;
+  ciudad: string;
+  estado: string;
+  cp: number;
+}
+
+export interface Producto {
+  id: number;
+  equipo: string;
+  precio: number;
+  cantidad: number;
+}
+
+export interface ResumenPedido {
+  productos: Producto[];
+  equipo: string;
+  subtotal: number;
+  envio: number;
+  total: number;
+  comentario: string;
+  direccion?: AddressData;
+}
